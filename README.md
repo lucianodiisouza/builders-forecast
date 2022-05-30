8f297c12850b7ae39ddb9fca1410a7cbbcbacb15
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-05-30 14:09:36 -0300

    feat: testing expo-location

---
+++ README.md
@@ -0,0 +1,15 @@
+## Builders Forecast
+
+This app was builted using React Native
+
+### libs
+
+- expo
+- axios
+- expo-location
+
+### dribbble design catched from
+
+Chris Iwan - [Dribbble](https://dribbble.com/Chrisiwan)
+
+https://dribbble.com/shots/7106462-Weather-App
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
+import * as Location from 'expo-location'
 
 const Home = () => {
+  const [location, setLocation] = useState<any>(null)
+
+  const getLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync()
+
+    if (status !== 'granted') {
+      console.warn('Sem permissões')
+      return
+    }
+
+    const location = await Location.getCurrentPositionAsync({})
+    setLocation(location)
+    console.warn(location)
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
   return (
     <View>
       <Text>Forecast</Text>
