e6af11f6cecef8c1fc8d5122ae773de3fecedc67
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-05-31 10:42:49 -0300

    feat: add goBack behaviour and change goback icon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Home, NextDays } from '../screens'
+import { Pressable } from 'react-native'
+import { Icon } from '../components'
+import { AppColors } from '../theme/GlobalStyles'
+import { useNavigation } from '@react-navigation/core'
 
 const Stack = createNativeStackNavigator()
 
-const NavigationStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name='Home'
-      component={Home}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen name='NextDays' component={NextDays} />
-  </Stack.Navigator>
-)
+const NavigationStack = () => {
+  const navigation = useNavigation()
+  return (
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen
+        name='Home'
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='NextDays'
+        component={NextDays}
+        options={{
+          headerTitle: '',
+          headerBackTitle: '',
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <Pressable onPress={() => navigation.goBack()}>
+              <Icon name='arrow-left' size={32} color={AppColors.DARK_BLUE} />
+            </Pressable>
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default NavigationStack
